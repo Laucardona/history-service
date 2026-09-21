e18bdde46cfb23a99ceca1fd0b4594d017f16880
Laucardona <lauris.042006@gmail.com>
2026-09-20 21:16:40 -0500

    feat: front

--- src/main/java/com/historial/config/CorsConfig.java
+++ src/main/java/com/historial/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200", "https://*.carvajal.com")
+                .allowedOriginPatterns("http://localhost:4200", "https://front-end-carvajal.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
